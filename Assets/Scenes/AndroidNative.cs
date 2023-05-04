d2e8c2d91734bc1cc2d85e4544202c569629cd71
Katopan <76249494+katomizuki@users.noreply.github.com>
2023-05-04 15:58:36 +0900

    add feature

--- Assets/Scenes/AndroidNative.cs
+++ Assets/Scenes/AndroidNative.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using UnityEngine;
+using UnityEngine.UI;
 public class AndroidNative : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private AndroidJavaObject _plugin;
+    private const string PackageName = "com.example.mylibrary";
+    private const string ClassName = "UnityAndroidPlugin";
+    [SerializeField] private Text _text;
+    
     void Start()
     {
-        
+       _plugin = new AndroidJavaObject($"{PackageName}.{ClassName}");
+       var text = _plugin.Call<int>("ReturnInt") + "Android確認";
+       _text.text = text;
+       Debug.Log(text);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+       _plugin.Dispose();
+       _plugin = null;
     }
 }
